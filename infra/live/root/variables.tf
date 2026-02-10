d89d7446ad7162ab97f36cbf68260f54104e95a7
abdikarim yusuf <abdikarimyusuf6@gmail.com>
2026-02-10 16:35:08 +0000

    run terraform fmt -check -recursive

--- infra/live/root/variables.tf
+++ infra/live/root/variables.tf
@@ -17,13 +17,13 @@ variable "project" {
 variable "environment" {
   type        = string
   description = "Environment name: dev, stage, prod"
-  default = "dev"
+  default     = "dev"
 }
 
 variable "domain_name" {
   type        = string
   description = "Base domain, "
-  default = "abdikarim.co.uk"
+  default     = "abdikarim.co.uk"
 }
 
 variable "sub_domain_name" {
@@ -33,23 +33,23 @@ variable "sub_domain_name" {
 
 # Networking
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 
 variable "availability_zones" {
-  type = list(string)
+  type    = list(string)
   default = ["eu-west-2a", "eu-west-2b"]
 }
 
