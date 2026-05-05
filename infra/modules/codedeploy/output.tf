91cc33af1c6dd31e3786c20e3e6760c426548ec7
abdikarim yusuf <abdikarimyusuf6@gmail.com>
2026-05-05 19:13:16 +0100

    fixed syntax on codeplay mode

--- infra/modules/codedeploy/output.tf
+++ infra/modules/codedeploy/output.tf
@@ -1,8 +1,8 @@
 output "app_name" {
-    value = aws_codedeploy_app.deploy.name
+    value = aws_codedeploy_app.cd-app.name
 }
 
 output "gp_name" {
-    value = aws_codedeploy_deployment_group.cd-app.deployment_group_name
+    value = aws_codedeploy_deployment_group.deploy.deployment_group_name
 }
 
